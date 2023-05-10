2bd0f0fd72fc83b0924ef4ba8c30946a00f2988d
Dann LeBeau <132018100+dannlebeau@users.noreply.github.com>
2023-05-10 14:51:03 -0400

    Add files via upload

--- assests/JS/script.js
+++ assests/JS/script.js
@@ -75,10 +75,16 @@ function abrirDiarioOficial(){
 function abrirleychile(){
   window.open("https://www.bcn.cl/leychile/");
 }  
- 
+
+function abrirhoyxhoy(){
+  window.open("https://www.hoyxhoy.cl/");
+}
+function abrirpublimetro(){
+  window.open("https://www.publimetro.cl/");
+}
  
  //Las Ultimas Noticias
- /*
+/* 
  (function() {
  function actualizarEnlace() {
     var fecha = new Date(document.getElementById("fecha").value);
@@ -129,8 +135,105 @@ function abrirleychile(){
     document.getElementById('fecha').value = fecha;
     actualizarEnlaces(); //link LUN
   } 
- })();  
-*/
+ })();*/
+// Esta opcion esta funcionando
+ (function() {
+  function actualizarEnlace() {
+    var fecha = new Date(document.getElementById("fecha").value);
+    var fecha_enlace = fecha.toISOString().slice(0, 10);
+    console.log(fecha_enlace);
+    document.getElementById("enlace").href =
+      "https://www.lun.com/Pages/NewsDetail.aspx?dt=" +
+      fecha_enlace +
+      "&EsAviso=0&PaginaId=1&bodyid=0";
+  }
 
-  
+  function actualizarImagen() {
+    var fecha = new Date(document.getElementById("fecha").value);
+    fecha.setUTCHours(0, 0, 0, 0); // establecer la zona horaria de la fecha a UTC
+    console.log("Fecha en UTC:", fecha.toUTCString());
+    console.log("Fecha en zona horaria local:", fecha.toLocaleString());
+    var mes = fecha.toLocaleString("default", { month: "short" });
+    var fecha_img =
+      fecha.getFullYear() +
+      "/" +
+      mes.toLowerCase() +
+      "/" +
+      (fecha.getDate() < 10 ? "0" : "") +
+      fecha.getDate();
+    console.log(fecha_img);
+
+    fecha.setDate(fecha.getDate() + 1); // Agregamos un día a la fecha
+    mes = fecha.toLocaleString("default", { month: "short" }); // Actualizamos el valor de mes
+
+    var dia = fecha.getDate().toString().padStart(2, "0");
+    var mes_num = (fecha.getMonth() + 1).toString().padStart(2, "0");
+
+    var url_final =
+      "https://images.lun.com/lunservercontents/NewsPaperPages/" +
+      fecha.getFullYear() +
+      "/" +
+      mes.toLowerCase() +
+      "/" +
+      dia +
+      "/LUCST01LU" +
+      dia +
+      mes_num +
+      "_550.jpg";
+    console.log(url_final);
+    document.getElementById("imagen").src = url_final;
+  }
+
+  function actualizarEnlaces() {
+    actualizarEnlace();
+    actualizarImagen();
+  }
+
+  function setearHoy() {
+    var hoy = new Date();
+    var dd = String(hoy.getDate()).padStart(2, "0");
+    var mm = String(hoy.getMonth() + 1).padStart(2, "0"); // Enero es 0
+    var yyyy = hoy.getFullYear();
+    var fecha = yyyy + "-" + mm + "-" + dd;
+    document.getElementById("fecha").value = fecha;
+    actualizarEnlaces(); //link LUN
+  }
+
+  setearHoy(); // Llama a la función setearHoy al cargar la página
+
+  // Asignar las funciones a los eventos correspondientes
+  document.getElementById("fecha").addEventListener("change", actualizarEnlaces);
+  document.getElementById("enlace").addEventListener("click", actualizarImagen);
+})();
+
+//Publimetro
+(function() {
+  function actualizarEnlace_2() {
+    var fecha = new Date(document.getElementById("fecha").value);
+    var dia = ("0" + (fecha.getDate() +1)).slice(-2);
+    var mes = ("0" + (fecha.getMonth() + 1)).slice(-2);
+    var anio = fecha.getFullYear();
+    var fecha_enlace = anio + mes + dia;
+    console.log(fecha_enlace);
+    var enlace = document.getElementById("enlace_2");
+    enlace.href = "https://www.readmetro.com/es/chile/santiago/" + fecha_enlace + "/1/";
+    enlace.target = "_blank";
+  }
+
+  // Resto del código
+
+  function actualizarEnlaces_1() {
+    actualizarEnlace_2();
+    // Resto de funciones para actualizar enlaces
+  }
+
+  // Resto del código
+
+  // Asignar las funciones a los eventos correspondientes
+  document.getElementById("fecha").addEventListener("change", actualizarEnlaces_1);
+  document.getElementById("enlace_2").addEventListener("click", actualizarEnlace_2);
+})();
+
+// La Tercera
+
 
